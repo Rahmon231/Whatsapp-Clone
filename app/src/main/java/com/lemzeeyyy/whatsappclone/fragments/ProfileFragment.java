0891e9518b46c08cdd3098071255ec1f30ed598e
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-24 23:10:08 +0100

    profile fragment

--- app/src/main/java/com/lemzeeyyy/whatsappclone/fragments/ProfileFragment.java
+++ app/src/main/java/com/lemzeeyyy/whatsappclone/fragments/ProfileFragment.java
@@ -1,17 +1,47 @@
 package com.lemzeeyyy.whatsappclone.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.lemzeeyyy.whatsappclone.R;
+import com.lemzeeyyy.whatsappclone.model.Users;
 
 
 public class ProfileFragment extends Fragment {
+    private TextView userName;
+    private ImageView profilePicture;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
 
 
     public ProfileFragment() {
@@ -21,15 +51,54 @@ public class ProfileFragment extends Fragment {
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        userName = view.findViewById(R.id.username_profile);
+        profilePicture = view.findViewById(R.id.profile_image2);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("MyUsers").child(firebaseUser.getUid());
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Users user = snapshot.getValue(Users.class);
+                userName.setText(user.getUsername());
+                if(user.getImageURL().equals("default")){
+                    profilePicture.setImageResource(R.mipmap.ic_launcher);
+                }else {
+                    Glide.with(getContext()).load(user.getImageURL()).into(profilePicture);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+        return view;
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+      if(requestCode == IMAGE_REQUEST && requestCode == RESULT_OK
+              && data!=null && data.getData()!=null){
+          imageUri = data.getData();
+      }
     }
 }
